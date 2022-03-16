4bdd392ed64c41cf371f969abbe92ebe5cbf937d
Vinicius Vielmo Cogo <vvcogo@fc.ul.pt>
2022-03-16 11:15:53 +0000

    Update sra_script.py

--- PrepareMetadata/sra_script.py
+++ PrepareMetadata/sra_script.py
@@ -22,7 +22,7 @@ def create_pattern_ion_torrent(row):
     return model_result
 
 #put every model from each platform in the same pattern
-def create_pattern_models(file,changes_models_dict,platform_model_dict,changes_models_dict):
+def create_pattern_models(file,changes_models_dict,platform_model_dict):
     aux = []
     for row in file.itertuples(index = False):
         platform = getattr(row,"Platform")
@@ -35,18 +35,18 @@ def create_pattern_models(file,changes_models_dict,platform_model_dict,changes_m
             model_result = create_pattern_ion_torrent(row).strip()
             aux.append([platform,model_result,run])
 
+            #if model_result not in changes_models_dict:
+            #    changes_models_dict[model_result] = model
             if model_result not in changes_models_dict:
-                changes_models_dict[model_result] = model
-            if model_result not in changes_models_dict:
-                changes_models_dict[model_result] = []
+                changes_models_dict[model_result] = [model]
             changes_models_dict[model_result].append([run,spots])
 
         if platform in platform_model_dict and model_before_whitespace in platform_model_dict.values():
             model_result = model.strip()
             aux.append([platform,model_result,run])
 
-            if model_result not in changes_models_dict:
-                changes_models_dict[model_result] = model
+            #if model_result not in changes_models_dict:
+            #    changes_models_dict[model_result] = model
             if model_result not in changes_models_dict:
                 changes_models_dict[model_result] = []
             changes_models_dict[model_result].append([run,spots])
@@ -55,8 +55,8 @@ def create_pattern_models(file,changes_models_dict,platform_model_dict,changes_m
             model_result =(platform_model_dict[platform] + " " + model).strip()
             aux.append([platform,model_result,run])
 
-            if model_result not in changes_models_dict:
-                changes_models_dict[model_result] = model
+            #if model_result not in changes_models_dict:
+            #    changes_models_dict[model_result] = model
             if model_result not in changes_models_dict:
                 changes_models_dict[model_result] = []
             changes_models_dict[model_result].append([run,spots])
@@ -71,7 +71,7 @@ def create_dir(file):
 
 def delete_dir(primary_path):
     #for deleting the results that we created using the argument -d
-    if len(argv) > 0 and argv[0] == '-d':
+    #if len(argv) > 0 and argv[0] == '-d':
         if os.path.exists(primary_path):
                 try:
                     shutil.rmtree(primary_path)
@@ -104,37 +104,42 @@ def create_files(primary_path,names_dict,changes_models_dict):
         df.to_csv('%s.csv' %names_dict[key],index = False)
 
 
-def main(argv):
+def main(args):
     #creating the main directory
-    primary_path = "Results"
+    #the output is placed in the directory passed as argv[1]
+    primary_path = args[1]
 
     delete_dir(primary_path)
 
     #runing the script normaly to create the results
-    else:
-        create_dir(primary_path)
-        #the platform as the keys and the beggining of the models as values
-        platform_model_dict = {'ABI_SOLID':'AB','BGISEQ':'','CAPILLARY':'AB','COMPLETE_GENOMICS':'Complete','HELICOS':'Helicos','ILLUMINA':'Illumina','LS454':'454','OXFORD_NANOPORE':'','PACBIO_SMRT':'PacBio'}
-        #the models after the changes as the keys and before the changes as the values
-        changes_models_dict = {} 
-        #the models alredy changed and with no repeated as keys and the list with the run and spots as the values
-        changes_models_dict = defaultdict(list)
-        #the models alredy changed as the keys and the names of the files( the path with the directory also) as the values
-        names_dict = {}
+    create_dir(primary_path)
+    #the platform as the keys and the beggining of the models as values
+    platform_model_dict = {'ABI_SOLID':'AB','BGISEQ':'','CAPILLARY':'AB','COMPLETE_GENOMICS':'Complete','HELICOS':'Helicos','ILLUMINA':'Illumina','LS454':'454','OXFORD_NANOPORE':'','PACBIO_SMRT':'PacBio'}
+    #the models after the changes as the keys and before the changes as the values
+    changes_models_dict = {}
+    #the models alredy changed and with no repeated as keys and the list with the run and spots as the values
+    changes_models_dict = defaultdict(list)
+    #the models alredy changed as the keys and the names of the files( the path with the directory also) as the values
+    names_dict = {}
 
-        df = pd.read_csv ('SraRunInfo.csv')
-        #getting the platform,model,run and spots
-        filtered_df = df.iloc[:,[0,3,18,19]]
-        #excluding the  rows names that apear in the middle of the file
-        final_filtered_df = filtered_df[(filtered_df["Platform"] != "Platform")]
-        final_filtered_df.to_csv(os.path.join(primary_path,'SraRunInfoReturn.csv'), index=False)
+    #the input CSV file is the one passed as argv[0]
+    df = pd.read_csv (args[0])
+    #getting the platform,model,run and spots
+    filtered_df = df.iloc[:,[0,3,18,19]]
+    #excluding the  rows names that apear in the middle of the file
+    final_filtered_df = filtered_df[(filtered_df["Platform"] != "Platform")]
+    final_filtered_df.to_csv(os.path.join(primary_path,'SraRunInfoReturn.csv'), index=False)
 
-        #make a csv file with the count of every model without the repeated ones
-        final_count = create_pattern_models(filtered_df,changes_models_dict,platform_model_dict,changes_models_dict).groupby(['Platform','Model']).count()
-        final_count.to_csv(os.path.join(primary_path,'SraRunInfoCount.csv'))
+    #make a csv file with the count of every model without the repeated ones
+    final_count = create_pattern_models(filtered_df,changes_models_dict,platform_model_dict).groupby(['Platform','Model']).count()
+    final_count.to_csv(os.path.join(primary_path,'SraRunInfoCount.csv'))
 
-        create_files(primary_path,names_dict,changes_models_dict)
+    create_files(primary_path,names_dict,changes_models_dict)
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    if len(sys.argv) == 3:
+        main(sys.argv[1:])
+    else:
+        print('Usage: python3 sra_script.py <input_CSV> <output_dir>')
+        return
