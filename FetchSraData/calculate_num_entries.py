f979b3c68c4965276471a40ac600d299bd64b5d2
luanaFOliveira <94805217+luanaFOliveira@users.noreply.github.com>
2022-04-04 14:07:16 -0300

    Update calculate_num_entries.py

--- FetchSraData/calculate_num_entries.py
+++ FetchSraData/calculate_num_entries.py
@@ -7,6 +7,10 @@ def main():
     percentage_spotsAvg = float(sys.argv[1])
     file_size = float(sys.argv[2])
     avgLength = int(sys.argv[3])
+    if percentage_spotsAvg == 0:
+        percentage_spotsAvg = 1
+    if avgLength == 0:
+        avgLength = 1
     size_run = (file_size * 1048576 * (percentage_spotsAvg/100) )
     num_entries = math.ceil(size_run/avgLength)
     print(num_entries)
