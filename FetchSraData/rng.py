89117bc6d3feb9d13eee3714284dfd6ea322f9ab
luanaFOliveira <94805217+luanaFOliveira@users.noreply.github.com>
2022-04-03 21:32:04 -0300

    Update rng.py

--- FetchSraData/rng.py
+++ FetchSraData/rng.py
@@ -2,11 +2,8 @@ import random
 import sys
 
 def main():
-    random.seed(int(sys.argv[3]))
-
-    for _ in range(int(sys.argv[4])):
-        if(int(sys.argv[2]) != 0):
-            print(sys.argv[1] + "," + str(random.randint(0, int(sys.argv[2]))))
+    random.seed(int(sys.argv[1]))
+    print(random.randint(0, int(sys.argv[2])))
 
 if __name__ == "__main__":
     main()
